97724aa784e73334a900745100d2efbb5685c1f6
Ludovic Lafole <ludovic.lafole@gmail.com>
2017-09-12 14:50:18 +0200

    log instead of stupidly crash when analytics don't load or are blocked

--- src/utils/initializeGA.js
+++ src/utils/initializeGA.js
@@ -4,8 +4,14 @@ export function initializeGA() {
     return lazyScripts('https://www.google-analytics.com/analytics.js')
         .then(() => {
             const ga = window.ga;
+            /** if it didn't load */
+            if (!ga) return noAnalytics();
 
             ga('create', 'UA-106348061-1', 'auto');
             ga('send', 'pageview');
-        });
+        }).catch(noAnalytics);
+}
+
+function noAnalytics() {
+    console.log("google analytics weren't loaded");
 }
