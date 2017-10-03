65944a22d62c35aec2bf7fec43b86bf6723880ff
Ludovic Lafole <ludovic.lafole@gmail.com>
2017-10-04 01:45:22 +0200

    add auto close responsive menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Promise from 'promise-polyfill';
 import { initializeGA } from './utils/initializeGA';
 import "intersection-observer";
 import lozad from 'lozad';
+import "./utils/menu"
 
 /** lightweight promise polyfill */
 if (!window.Promise) window.Promise = Promise;
