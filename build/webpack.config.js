8123350edb0fdd22b303bfc8b6287336664238bd
Ludovic Lafole <ludovic.lafole@gmail.com>
2017-11-26 17:29:46 +0100

    Fixes PR deploy path

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -9,8 +9,8 @@ const CleanPlugin = require('clean-webpack-plugin')
 const ImageOptimizePlugin = require('imagemin-webpack-plugin').default
 
 const devMode = process.env.NODE_ENV !== 'production'
-const isPR = process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST != "false"
-const prNumber = process.env.TRAVIS_PULL_REQUEST
+const isPR = process.env.ghprbPullId && process.env.ghprbPullId != "false"
+const prNumber = process.env.ghprbPullId
 
 
 // To make LinkMediaHtmlWebpackPlugin add automatically the media...
