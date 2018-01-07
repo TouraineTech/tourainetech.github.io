d00e34ebd2a0408bdb4ceb75b9949393b7a8619c
Ludovic Lafole <ludovic.lafole@gmail.com>
2018-01-08 00:27:44 +0100

    reoganize the code

--- src/articles/articles.js
+++ src/articles/articles.js
@@ -1,22 +1,10 @@
-const moment = require('moment')
-const mdRequire = require.context('./', true, /\.md/)
+import { compareArticlesByDate } from './utils/compareArticlesByDate'
+import { articlesDatesToMomentDates } from './utils/articlesDatesToMomentDates'
+
+const mdRequire = require.context('./content', true, /\.md/)
 
 module.exports = mdRequire
     .keys()
     .map(mdRequire)
-    .map((a) => ({
-        ...a,
-        creationDate: moment(a.meta.creationDate),
-        updateDate  : moment(a.meta.updateDate),
-    }))
-    .sort(function (a1, a2) {
-        const creationDate1 = a1.creationDate
-        const creationDate2 = a2.creationDate
-        if(creationDate1.isSame(creationDate2)) {
-            return 0
-        } else if (creationDate1.isAfter(creationDate2)) {
-            return -1
-        } else {
-            return 1
-        }
-    })
+    .map(articlesDatesToMomentDates)
+    .sort(compareArticlesByDate)
