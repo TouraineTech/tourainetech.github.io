51d6f55c88f297116d96ab79df349dc17fa3b7c7
Benjamin Cousin <b.cousin@code-troopers.com>
2018-10-09 12:30:08 +0200

    feat(cfp): fix dates

--- components/CFP.vue
+++ components/CFP.vue
@@ -3,7 +3,7 @@
     <div class="container--fix container--center">
       <h2>Call for paper</h2>
       <p>
-        Vous pouvez proposer un sujet jusqu'au <span class="label--text label--green">dimanche 7 décembre 2018</span>, la délibération se fera le <span class="no-wrap">X décembre 2019</span>.
+        Vous pouvez proposer un sujet jusqu'au <span class="label--text label--green">dimanche 9 décembre 2018</span>, la délibération se fera le <span class="no-wrap">dimanche 23 décembre 2019</span>.
       </p>
       <p>
         <a class="button--link button--green" href="https://touraine-tech-cfp.firebaseapp.com/speaker" target="_blank">Proposez vos sujets !</a>
