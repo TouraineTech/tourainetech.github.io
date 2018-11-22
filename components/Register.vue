e477648bf65fb09ed3cb3d8d7bd76508ec69faae
Benjamin Cousin <b.cousin@code-troopers.com>
2018-11-22 19:33:01 +0100

    chore(register): add help phrase to redirect users to helloasso website

--- components/Register.vue
+++ components/Register.vue
@@ -9,7 +9,16 @@
               style="width:100%;height:900px;border:none;">
 
       </iframe>
-      <div style="width:100%;text-align:center;">Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a></div>
+      <div>Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a></div>
+      <div class="helper">Si vous n'arrivez pas à réserver, essayez <a target="_blank" href="https://www.helloasso.com/associations/palo-altours/evenements/touraine-tech-2019/" rel="nofollow">sur le site HelloAsso</a> ou envoyez-nous un mail à <a href="mailto:team@touraine.tech">team@touraine.tech</a></div>
     </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+  .helper {
+    margin-top: 1rem;
+    font-style: italic;
+    font-size: 0.8rem;
+  }
+</style>
