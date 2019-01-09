ebad2dd5251ddb4f0078e594133f34afa0f800e3
Benjamin Cousin <b.cousin@code-troopers.com>
2019-01-09 10:27:33 +0100

    fix(map): fix quote in map url

--- components/Map.vue
+++ components/Map.vue
@@ -7,7 +7,7 @@
       style="border:0"
       title="Map of the event"
       allowfullscreen
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2702.4214613291897!2d0.6825578166711853!3d47.36469207916962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd6799ba64a61%3A0x178da484f4c36cd4!2sUniversit%C3%A9+de+Tours+-+Ecole+d&#39;Ing%C3%A9nieur+Polytechnique!5e0!3m2!1sfr!2sfr!4v1546945349918"></iframe>
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2702.4214613291897!2d0.6825578166711853!3d47.36469207916962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd6799ba64a61%3A0x178da484f4c36cd4!2sUniversit%C3%A9+de+Tours+-+Ecole+dIng%C3%A9nieur+Polytechnique!5e0!3m2!1sfr!2sfr!4v1546945349918"></iframe>
   </section>
 </template>
 
