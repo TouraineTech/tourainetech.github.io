00cf03f36f9c5aa6b69d450764baf8d67fb921c6
Benjamin Cousin <b.cousin@code-troopers.com>
2019-01-18 15:33:25 +0100

    feat(register): soldout

--- components/Register.vue
+++ components/Register.vue
@@ -2,15 +2,7 @@
   <section id="register" class="container--green">
     <div class="container--fix container--center">
       <h2>Inscriptions</h2>
-      <iframe id="haWidget"
-              allowtransparency="true"
-              scrolling="auto"
-              src="https://www.helloasso.com/associations/palo-altours/evenements/touraine-tech-2019/widget"
-              style="width:100%;height:900px;border:none;">
-
-      </iframe>
-      <div>Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a></div>
-      <div class="helper">Si vous n'arrivez pas à réserver, essayez <a target="_blank" href="https://www.helloasso.com/associations/palo-altours/evenements/touraine-tech-2019/" rel="nofollow">sur le site HelloAsso</a> ou envoyez-nous un mail à <a href="mailto:team@touraine.tech">team@touraine.tech</a></div>
+      <p>C'est COMPLET, rendez-vous l'année prochaine !</p>
     </div>
   </section>
 </template>
