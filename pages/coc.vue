144603386f3fc7e8658438ec5d3ddb9b62a9ef84
florianroulet <florianroulet@gmail.com>
2023-01-16 17:31:21 +0100

    feat(coc) : add own CoC

---
+++ pages/coc.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="page--coc container--white">
+    <h1>Code de conduite pour la conférence</h1>
+
+    <p>
+      Toutes les personnes participant à la conférence,
+      y compris sponsors et bénévoles
+      s'engagent à accepter le code d'éthique et de déontologie, ou « code de conduite » suivant.
+      L'organisation s'attachera à faire respecter ce code durant l'événement.
+      Nous attendons la coopération de chacun‧e pour assurer un environnement sain pour tou‧te‧s.
+    </p>
+
+    <h2>La version rapide</h2>
+
+    <p>
+      Notre conférence se veut une expérience sans harcèlement, quel que soit
+      votre sexe, votre identité sexuelle, votre âge, votre orientation
+      sexuelle, votre handicap, votre apparence physique, votre poids, votre
+      race ou votre religion. Nous ne tolérons aucun harcèlement des
+      participant‧e‧s à la conférence, sous quelque forme que ce soit.
+      Les expressions et les images à connotation sexuelle ne sont pas appropriées
+      lors de l'événement. Ceci inclut les conférences, les ateliers, les soirées,
+      Twitter et les autres médias en ligne.
+      L'organisation s'engage à prendre des sanctions appropriées pour les personnes
+      qui violent ces règles, jusqu'à l'exclusion <em>sans remboursement</em>
+      et le recours légal.
+    </p>
+
+    <h2>La version moins rapide</h2>
+
+    <p>
+      Le harcèlement inclut des commentaires à l'oral ou à l'écrit sur le sexe,
+      l'identité sexuelle, l'âge, l'orientation sexuelle, le handicap, l'apparence
+      physique, le poids, la race, la religion, les images à connotation
+      sexuelle dans des lieux publics, les intimidations délibérées, la traque,
+      la poursuite, un harcèlement photographique ou vidéo, une suite
+      d'interruptions des conférences et des autres événements, un contact
+      physique inapproprié et des avances sexuelles non désirées.
+    </p>
+
+    <p>
+      Les participant‧e‧s à qui il sera demandé d'arrêter tout comportement de
+      harcèlement doivent arrêter immédiatement.
+    </p>
+
+    <p>
+      Les sponsors sont aussi sujet à la politique anti-harcèlement. En
+      particulier, les sponsors ne doivent pas utiliser d'images ou de supports
+      à connotation sexuelle. Ils ne doivent pas non plus se livrer à des activités
+      à connotation sexuelle. L'équipe du stand (y compris les bénévoles) ne
+      doit pas utiliser de vêtements, uniformes ou costumes à connotation
+      sexuelle. Ils ne doivent pas non plus créer un environnement sexualisé.
+    </p>
+
+    <p>
+      Face à un comportement de harcèlement, l'équipe d'organisation de la conférence
+      peut prendre toute action qui lui semble adéquate.
+      Cela va d'un simple avertissement à l'exclusion sans remboursement de la conférence.
+    </p>
+
+    <p>
+      Si vous vous sentez harcelé‧e, si vous pensez que quelqu'un se fait
+      harceler, et plus généralement en cas de problème, merci de contacter
+      immédiatement l’équipe d’organisation de l'événement. L'organisation s'assure
+      que ses membres sont facilement identifiables (badge, brassard, tour de cou,
+      t-shirt…) présentés au début de la conférence.
+    </p>
+
+    <p>
+      Les membres de l'organisation seront ravi‧e‧s d'aider les participants à
+      contacter la sécurité de l'hôtel ou du bâtiment où se déroule l'événement,
+      ou les forces de l'ordre ; à fournir une escorte ainsi qu'à aider de toute
+      autre façon les personnes victimes de harcèlement, pour garantir leur
+      sécurité pendant la durée de l'événement. Nous apprécions votre
+      participation à l'événement.
+    </p>
+
+    <p>
+      Nous attendons de chacun‧e le respect de ces règles dans le bâtiment
+      des conférences et des ateliers, ainsi que pendant les événements sociaux
+      relatifs à la conférence.
+    </p>
+
+    <p>
+      En cas souci, merci de vous diriger vers <a href="https://touraine.tech/#team">un membre de l'équipe d'organisation</a> en sweat vert.
+    </p>
+
+    <div class="footer">
+      <p>
+        <small>
+          <em>
+            Texte repris depuis <a href="https://confcodeofconduct.com/index-fr.html">confcodeofconduct.com</a>
+
+            Version originale et crédit:
+            <a href="http://2012.jsconf.us/#/about">http://2012.jsconf.us/#/about</a>
+            &amp;
+            <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">The Ada Initiative</a><br />
+            Merci d'aider à la traduction et à l'amélioration de ce texte :
+            <a href="https://github.com/confcodeofconduct/confcodeofconduct.com">on github.com</a><br />
+
+            Ce texte est sous licence
+            <a
+              rel="license"
+              href="http://creativecommons.org/licenses/by/3.0/deed.en_US"
+            >Creative Commons Attribution 3.0 Unported License</a>
+          </em>
+        </small>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.page--coc {
+  width: 60%;
+  text-align: justify;
+  margin: 3rem auto 1rem;
+}
+.footer {
+  margin-top: 1rem;
+}
+</style>
