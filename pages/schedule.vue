8803b3a0445066cd334bf1ea2184e4200613b095
Benjamin COUSIN <benou.cousin@gmail.com>
2024-01-17 09:51:14 +0100

    feat(schedule): add a naked layout without header and smartbanner for app

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="page--schedule">
-    <Schedule/>
+    <Schedule :for-app="true" />
   </div>
 </template>
 
 <script>
   import Schedule from '~/components/Schedule'
-  
+
   export default {
     components: {
       Schedule
     },
+    layout: 'naked',
   }
 </script>
 
 <style lang="scss" scoped>
 .page--schedule {
-  margin-top: 2rem;
+  margin-top: -2rem;
 }
 </style>
