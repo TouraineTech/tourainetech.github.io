2ee113246f70b339763d540a158c4b4a13bf14c7
florianroulet <florianroulet@gmail.com>
2024-12-20 15:27:02 +0100

    feat(openStreetMap) : avoid gMaps cookie by using openStreetMap instead (no cookie used) (#420)

--- components/Map.vue
+++ components/Map.vue
@@ -3,11 +3,10 @@
     <iframe
       width="100%"
       height="400px"
-      frameborder="0"
       style="border:0"
       title="Map of the event"
       allowfullscreen
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1351.4019759669725!2d0.7032385!3d47.3572254!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd70053e4f879%3A0x31be2c02e60c1d82!2sUFR%20des%20sciences%20et%20techniques!5e0!3m2!1sfr!2sfr!4v1731408085815!5m2!1sfr!2sfr"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=0.7009148597717286%2C47.35612733022247%2C0.7038250565528871%2C47.35843127917653&amp;layer=mapnik&amp;marker=47.35727931727454%2C0.7023699581623077"
     ></iframe>
   </section>
 </template>
