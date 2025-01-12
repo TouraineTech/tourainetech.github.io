4b55de8b3e838e0b61e8801d8bde41ef21a56017
Florian Roulet <florianroulet@gmail.com>
2025-01-12 18:14:14 +0100

    feat(captationRegie) : add a new page to remotely control all four OBS computer and monitor youtube streaming

---
+++ components/RegieRoom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="regie--room">
+    <iframe
+      allowfullscreen
+      src="http://obs-web.niek.tv/"
+    ></iframe>
+    <iframe
+      allowfullscreen
+      src="https://www.youtube.com/embed/g79SX2lU8Gw"
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.regie--room>iframe {
+  width: 49%;
+  height: 100%;
+}
+</style>
---
+++ pages/regie/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container--white">
+    Bienvenue sur la régie TNT
+    <section id="regie" class="container--grid">
+      <RegieRoom />
+      <RegieRoom />
+      <RegieRoom />
+      <RegieRoom />
+    </section>
+  </div>
+</template>
+
+<script>
+
+import RegieRoom from "@/components/RegieRoom.vue";
+
+export default {
+  components: {
+    RegieRoom
+  },
+  mounted() {
+    setTimeout(() => {
+      window.scroll({top: document.getElementById("regie").offsetTop});
+    }, 2000);
+  }
+}
+</script>
+
+<style>
+.container--grid{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0.1vw;
+  grid-auto-rows: 50vh;
+  width: 100%;
+  height: 100vh;
+}
+.regie--room{
+ width: 50vw;
+ height: 50vh;
+}
+</style>
