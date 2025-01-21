1a60c5c46904f11405bfb12263dfe31a8013e75a
florianroulet <florianroulet@gmail.com>
2025-01-21 08:20:12 +0100

    feat(captationRegie) : add a new page to remotely control all four OBS computer and monitor youtube streaming (#426)
    * feat(captationRegie) : add a new page to remotely control all four OBS computer and monitor youtube streaming
    
    * feat(captationRegie) : add parameters on the regie page for each youtube video code and define ngrok websocket urls
    
    * feat(captationRegie) : forgotten obs-web link
    
    * feat(captationRegie) : use TNT fork for obs-web

---
+++ components/RegieRoom.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="regie--room">
+    <iframe
+      allowfullscreen
+      :src="obs"
+    ></iframe>
+    <iframe
+      allowfullscreen
+      :src="youtube"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    obs: {
+      type: String,
+      default() {
+        return "";
+      }
+    },
+    youtube: {
+      type: String,
+      default() {
+        return "";
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.regie--room>iframe {
+  width: 49%;
+  height: 100%;
+}
+</style>
---
+++ pages/regie/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container--white">
+    <h1>Bienvenue sur la régie TNT</h1>
+    <p>Passer les paramètres des url youtube comme le lien suivant :</p>
+    <p>https://touraine.tech/regie?F21=codeVideoYoutubeSalleF21&F22=codeVideoYoutubeSalleF22&bio=codeVideoYoutubeSalleBio&physique=codeVideoYoutubeSallePhysique</p>
+    <section id="regie" class="container--grid">
+      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://starfish-fair-unicorn.ngrok-free.app/'" :youtube="F21" />
+      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://labrador-happy-unduly.ngrok-free.app/'" :youtube="F22" />
+      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://innocent-python-premium.ngrok-free.app/'" :youtube="bio" />
+      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://vital-raccoon-preferably.ngrok-free.app/'" :youtube="physique" />
+    </section>
+  </div>
+</template>
+
+<script>
+import RegieRoom from "@/components/RegieRoom.vue";
+
+export default {
+  components: {
+    RegieRoom
+  },
+  data: () => ({
+    F21:"",
+    F22:"",
+    bio:"",
+    physique:""
+  }),
+  mounted() {
+    setTimeout(() => {
+      window.scroll({top: document.getElementById("regie").offsetTop});
+    }, 2000);
+
+    this.F21 = `https://www.youtube.com/embed/${this.$route.query.F21}`;
+    this.F22 = `https://www.youtube.com/embed/${this.$route.query.F22}`;
+    this.bio = `https://www.youtube.com/embed/${this.$route.query.bio}`;
+    this.physique = `https://www.youtube.com/embed/${this.$route.query.physique}`;
+  }
+}
+</script>
+
+<style>
+.container--grid{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0.1vw;
+  grid-auto-rows: 50vh;
+  width: 100%;
+  height: 100vh;
+}
+</style>
