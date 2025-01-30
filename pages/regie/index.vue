59440fce0e6531c7c9d30e75c3b0090b35d1c750
Antoine Giret <ant.giret@gmail.com>
2025-01-30 16:58:53 +0100

    feat(captationRegie) : use responsive design on captation regie

--- pages/regie/index.vue
+++ pages/regie/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container--white">
-    <h1>Bienvenue sur la régie TNT</h1>
-    <p>Passer les paramètres des url youtube comme le lien suivant :</p>
-    <p>https://touraine.tech/regie?F21=codeVideoYoutubeSalleF21&F22=codeVideoYoutubeSalleF22&bio=codeVideoYoutubeSalleBio&physique=codeVideoYoutubeSallePhysique</p>
-    <section id="regie" class="container--grid">
-      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://starfish-fair-unicorn.ngrok-free.app/'" :youtube="F21" />
-      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://labrador-happy-unduly.ngrok-free.app/'" :youtube="F22" />
-      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://innocent-python-premium.ngrok-free.app/'" :youtube="bio" />
-      <RegieRoom :obs="'https://touraine.tech/obs-web/#wss://vital-raccoon-preferably.ngrok-free.app/'" :youtube="physique" />
-    </section>
+    <div id="container--regie">
+      <div>
+        <h1>Bienvenue sur la régie TNT</h1>
+        <p>Passer les paramètres des url youtube comme le lien suivant :</p>
+        <p class="scrollable">https://touraine.tech/regie?F21=codeVideoYoutubeSalleF21&F22=codeVideoYoutubeSalleF22&bio=codeVideoYoutubeSalleBio&physique=codeVideoYoutubeSallePhysique</p>
+      </div>
+      <section id="regie">
+        <RegieRoom :title="'F21'" :obs="'https://touraine.tech/obs-web/#wss://starfish-fair-unicorn.ngrok-free.app/'" :youtube="F21" />
+        <RegieRoom :title="'F22'" :obs="'https://touraine.tech/obs-web/#wss://labrador-happy-unduly.ngrok-free.app/'" :youtube="F22" />
+        <RegieRoom :title="'Bio'" :obs="'https://touraine.tech/obs-web/#wss://innocent-python-premium.ngrok-free.app/'" :youtube="bio" />
+        <RegieRoom :title="'Physique'" :obs="'https://touraine.tech/obs-web/#wss://vital-raccoon-preferably.ngrok-free.app/'" :youtube="physique" />
+      </section>
+    </div>
   </div>
 </template>
 
@@ -27,7 +31,7 @@ export default {
   }),
   mounted() {
     setTimeout(() => {
-      window.scroll({top: document.getElementById("regie").offsetTop});
+      window.scroll({top: document.getElementById("regie").offsetTop - 24});
     }, 2000);
 
     this.F21 = `https://www.youtube.com/embed/${this.$route.query.F21}`;
@@ -39,12 +43,28 @@ export default {
 </script>
 
 <style>
-.container--grid{
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 0.1vw;
-  grid-auto-rows: 50vh;
-  width: 100%;
-  height: 100vh;
+#container--regie {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 24px;
+}
+p.scrollable {
+  overflow-x: scroll;
+}
+#regie {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+}
+.regie--room {
+  height: calc((100dvh - 72px) / 2);
+  width: calc((100% - 24px) / 2);
+}
+@media only screen and (max-width: 1600px) {
+  .regie--room {
+    height: calc(100dvh - 48px);
+    width: 100%;
+  }
 }
 </style>
